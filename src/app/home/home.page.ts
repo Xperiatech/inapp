7ad471aaea4e920a9ea9b9551df75bcc3d6f468b
Junaid Satti <junaidsatti@Junaids-MacBook-Pro.local>
2020-11-10 12:42:03 +0500

    JGS

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
-
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  helpUrl= "https://www.google.com";
+   url = this.helpUrl.replace("watch?v=", "v/");
 
-  constructor(private iab: InAppBrowser) {
-    const browser = this.iab.create('https://mybrowser-search.com/search?q=inapp%20capacitor&ac=1468047342');
+  constructor(private iab: InAppBrowser,
+    private sanitizer: DomSanitizer) {
+   let ab:InAppBrowserOptions;
+   ab = {
+        footer:'no',
+        location:'no',
+        zoom:'no',
+
+      }
+    const browser = this.iab.create('https://mybrowser-search.com/search?q=inapp%20capacitor&ac=1468047342',"_self",ab);
     browser.show();
-  }
 
+  }
 }
